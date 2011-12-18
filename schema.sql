9e1a5ad89e85067bf6af8cd5228d85025fb5e889
Brock Sides <philarete@gmail.com>
2011-12-17 23:30:21 -0600

    Added north_south and east_west tables to schema.sql.

--- schema.sql
+++ schema.sql
@@ -5,3 +5,8 @@ CREATE TABLE Room (id integer,
                    gameid integer references Game(id),
                    description text,
                    primary key (id, gameid));
+CREATE TABLE north_south ( north integer unique,
+                           south integer unique );
+CREATE TABLE east_west ( east integer unique,
+                         west integer unique );
+                           
