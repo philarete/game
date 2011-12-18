80fe23b59484d4820911f042c679740cb1cf3fd1
Brock Sides <philarete@gmail.com>
2011-12-18 00:43:25 -0600

    Fixed schema so Room(gameid) references Game(gameid).

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE Game (gameid integer primary key,
                    id integer default 0, -- should always be zero
                    description text);
 CREATE TABLE Room (id integer, 
-                   gameid integer references Game(id),
+                   gameid integer references Game(gameid),
                    description text,
                    primary key (id, gameid));
 CREATE TABLE north_south ( north integer unique,
