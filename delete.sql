626a4adf986e6e28186321bd84292379b2e8008c
Brock Sides <philarete@gmail.com>
2011-12-19 21:00:11 -0600

    Added Character and Monster tables.

--- delete.sql
+++ delete.sql
@@ -1,2 +1,4 @@
 delete from Room;
 delete from Game;
+delete from Character;
+delete from Monster;
--- schema.sql
+++ schema.sql
@@ -9,4 +9,19 @@ CREATE TABLE Room (id integer not null,
                    south integer,
                    east integer,
                    west integer,
+                   monster integer,
                    primary key (id, gameid));
+CREATE TABLE Character (id integer not null, 
+                        gameid integer references Game(gameid),
+                        description text,
+                        location integer,
+                        hp integer, -- hit points
+                        damage integer, -- damage when attacking
+                        primary key (id, gameid));
+CREATE TABLE Monster (id integer not null, 
+                      gameid integer references Game(gameid),
+                      description text,
+                      location integer,
+                      hp integer, -- hit points
+                      damage integer, -- damage when attacking
+                      primary key (id, gameid));
